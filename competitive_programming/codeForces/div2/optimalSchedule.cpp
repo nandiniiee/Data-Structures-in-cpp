1379c8f04114296ade9fc46429ae154b6501d940
nandiniee <jain.nandini.squash@gmail.com>
2026-05-18 20:29:12 +0530

    code for optimal scheuling added

---
+++ competitive_programming/codeForces/div2/optimalSchedule.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t; // t is the number of test cases
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        long long int n; // n is number of students
+        long long int a; // a is the cost of individual schedule
+        long long int b; // b is the cost of the group schedule
+        cin >> n;
+        cin >> a;
+        cin >> b;
+        if (n <= 3)
+        {
+            if (a * n < b)
+            {
+                cout << a * n << endl;
+            }
+            else
+            {
+                cout << b << endl;
+            }
+        }
+        else
+        {
+            if (a * 3 < b)
+            {
+                cout << a * n << endl;
+            }
+            else
+            {
+                cout << b * (n / 3) + (n % 3) * a << endl;
+            }
+        }
+    }
+}
