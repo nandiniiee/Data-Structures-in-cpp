8d26b5cfe856b93925734aaf2494434205a7a905
nandiniee <jain.nandini.squash@gmail.com>
2026-05-30 20:46:37 +0530

    added cakeLeveling code

---
+++ competitive_programming/convergence.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t;    // t is the number of test cases
+    cin >> t; // read the number of test cases
+    for (int i = 0; i < t; i++)
+    {
+        int n; // n is the number of friends alice has
+        cin >> n;
+        vector<int> location(n); // location of each friend
+        for (int j = 0; j < n; j++)
+        {
+            cin >> location[j];
+        }
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
